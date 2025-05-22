baedfca42d662663b8c22c3c5f53da4e8aa38ae2
Antuuan <antonycheng2007@gmail.com>
2025-05-22 12:44:34 +0100

    users table creation

--- install.php
+++ install.php
@@ -19,3 +19,23 @@ $sql = "USE shop";
 $conn->exec($sql);
 echo "DB created successfully";
 
+//stores the SQL statement as a variable
+$statement=$conn->prepare("
+DROP TABLE IF EXISTS tbl_users;
+CREATE TABLE tbl_users
+(user_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(100) NOT NULL,
+username VARCHAR(50) NOT NULL,
+phone_no VARCHAR(11) NOT NULL,
+address VARCHAR(100) NOT NULL,
+postcode VARCHAR(7) NOT NULL,
+card_no VARCHAR(300) NOT NULL,
+card_name VARCHAR(300) NOT NULL,
+card_expiry VARCHAR(5) NOT NULL,
+cvc VARCHAR(300) NOT NULL,
+");
+
+//executes the SQL statement
+$statement->execute();
+$statement->closeCursor();
+echo "users table created"
