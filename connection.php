5700cd8a3878b772edffd54e50231f38cdfc9358
Antuuan <antonycheng2007@gmail.com>
2025-05-22 12:31:12 +0100

    added comments

--- connection.php
+++ connection.php
@@ -3,13 +3,16 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "shop";
+//variables for connecting to DB
 
 try {
+    //connects to DB
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
+    //set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo"Connected Successfully";
 }
+//if the try doesnt work
 catch(PDOException $e){
     echo"Connection Failed: " . $e->getMessage();
 }
--- install.php
+++ install.php
@@ -3,14 +3,18 @@
 $servername = 'localhost';
 $username = 'root';
 $password= '';
-//note no Database mentioned here!!
+//variables for connecting to server, no database name as it's not created yet
 
+//connects to server
 $conn = new PDO("mysql:host=$servername", $username, $password);
 // set the PDO error mode to exception
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//creates the database using SQL
 $sql = "CREATE DATABASE IF NOT EXISTS shop";
 $conn->exec($sql);
-//next 3 lines optional only needed really if you want to go on an do more SQL here!
+
+//selects the databese and message to notify database creation was successful
 $sql = "USE shop";
 $conn->exec($sql);
 echo "DB created successfully";
