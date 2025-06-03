2748867cc47b08ec295914e2b53b75d44530b537
Antuuan <antonycheng2007@gmail.com>
2025-06-03 09:45:45 +0100

    started navbar

--- index.php
+++ index.php
@@ -10,3 +10,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+<body>
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+        <!-- insert logo here -->
+    </div>
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item"><button type="button">ACCOUNT</button></li>
+                <li class="nav-item"><a href="login.php"><button type="button">LOGIN</button></a></li>
+                <li class="nav-item"><a href="signUp.php"><button type="button">SIGNUP</button></a></li>
+                <li class="nav-item"><a href="Books.php"><button type="button">ADD BOOK</button></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+</body>
