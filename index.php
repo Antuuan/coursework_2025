ad1607f97ff6f0a0e1fd30ebb1578a093b6be030
Antuuan <antonycheng2007@gmail.com>
2025-06-03 09:27:35 +0100

    finished intall page and made index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title>屌炸天网购</title>
+    <!-- importing bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="mystyle.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
--- install.php
+++ install.php
@@ -19,6 +19,7 @@ $sql = "USE shop";
 $conn->exec($sql);
 echo "DB created successfully";
 
+//users table
 //stores the SQL statement as a variable
 $statement=$conn->prepare("
 DROP TABLE IF EXISTS tbl_users;
@@ -32,10 +33,75 @@ postcode VARCHAR(7) NOT NULL,
 card_no VARCHAR(300) NOT NULL,
 card_name VARCHAR(300) NOT NULL,
 card_expiry VARCHAR(5) NOT NULL,
-cvc VARCHAR(300) NOT NULL,
+cvc VARCHAR(300) NOT NULL);
 ");
 
 //executes the SQL statement
 $statement->execute();
 $statement->closeCursor();
-echo "users table created"
+echo "<br>users table created";
+
+//order table
+//stores the SQL statement as a variable
+$statement=$conn->prepare("
+DROP TABLE IF EXISTS tbl_order;
+CREATE TABLE tbl_order
+(order_id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+status TINYINT(1) NOT NULL,
+seller_id INT(6) NOT NULL,
+buyer_id INT(6) NOT NULL,
+review TEXT,
+stars TINYINT(1) NOT NULL);
+");
+
+//executes the SQL statement
+$statement->execute();
+$statement->closeCursor();
+echo "<br>order table created";
+
+//order contents table
+//stores the SQL statement as a variable
+$statement=$conn->prepare("
+DROP TABLE IF EXISTS tbl_order_contents;
+CREATE TABLE tbl_order_contents
+(order_id INT(10) NOT NULL,
+item_id INT(10) NOT NULL,
+qty TINYINT(2) NOT NULL);
+");
+
+//executes the SQL statement
+$statement->execute();
+$statement->closeCursor();
+echo "<br>order_contents table created";
+
+//items table
+//stores the SQL statement as a variable
+$statement=$conn->prepare("
+DROP TABLE IF EXISTS tbl_items;
+CREATE TABLE tbl_items
+(item_id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+seller_id INT(6) NOT NULL,
+status TINYINT(1) NOT NULL,
+price FLOAT(10) NOT NULL,
+start_date VARCHAR(8) NOT NULL,
+end_date VARCHAR(8) NOT NULL);
+");
+
+//executes the SQL statement
+$statement->execute();
+$statement->closeCursor();
+echo "<br>items table created";
+
+//admins table
+//stores the SQL statement as a variable
+$statement=$conn->prepare("
+DROP TABLE IF EXISTS tbl_admins;
+CREATE TABLE tbl_admins
+(username VARCHAR(30) NOT NULL,
+password VARCHAR(100) NOT NULL);
+");
+
+//executes the SQL statement
+$statement->execute();
+$statement->closeCursor();
+echo "<br>admins table created";
