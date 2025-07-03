9c045c7945bb98692e31878fcd6be9c5a6408c86
Antuuan <antonycheng2007@gmail.com>
2025-07-03 16:27:08 +0100

    added comments

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+<!-- flex-column to allow the rows to stack vertically -->
 <nav class="navbar navbar-expand-lg flex-column">
+    <!-- w-100 class makes the row and container take up 100% of the screen width -->
     <div class="container-fluid w-100">
         <div class="row w-100">
             <div class="col text-center">
@@ -22,12 +24,16 @@
     <div class="container-fluid w-100">
         <div class="row w-100">
             <div class="col w-100">
-                <div class="container-fluid text-center">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <!-- using text-start to make the button on the left -->
+                <div class="container-fluid text-start">
+                    <!-- hamburger button, navbar_nav here links to the navbar_nav in the collapse menu -->
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar_nav"
+                        aria-controls="navbar_nav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbar_nav">
+                    <!-- collapse and navbar-collapse means this div is collapseable -->
+                    <!-- using text-center to make the buttons centered once the menu opens -->
+                    <div class="collapse navbar-collapse text-center" id="navbar_nav">
                         <ul class="navbar-nav mx-auto">
                             <li class="nav-item"><button type="button">ACCOUNT</button></li>
                             <li class="nav-item"><a href="login.php"><button type="button">LOGIN</button></a></li>
