71656ea89ee9bcadc3e24a832e6270ae66249dc1
Antuuan <antonycheng2007@gmail.com>
2025-10-19 16:07:28 +0100

    pictures working

--- index.php
+++ index.php
@@ -14,24 +14,24 @@ include("navbar.php")
     // joins all 3 tables to allow you to access individual things from each table
 	include_once('connection.php');
 	$stmt = $conn->prepare("
-    SELECT tbl_items.item_name as ItName, tbl_items.item_description as Itdesc, tbl_items.price as Itprice,
-    tbl_pics.image_name as imname
-    FROM tbl_items_n_pics
-    INNER JOIN tbl_items
-    ON tbl_items.item_id = tbl_items_n_pics.item_id
-    INNER JOIN tbl_pics
+    SELECT tbl_items.item_name as ItName, tbl_items.item_description as Itdesc, tbl_items.price as Itprice, 
+    GROUP_CONCAT(tbl_pics.image_name) AS imageurls 
+    FROM tbl_items_n_pics 
+    INNER JOIN tbl_items 
+    ON tbl_items.item_id = tbl_items_n_pics.item_id 
+    INNER JOIN tbl_pics 
     ON tbl_pics.pic_id = tbl_items_n_pics.pic_id
     ");
-	$stmt->execute();	
+	$stmt->execute();
     
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-    {
-        #print_r($row);
-        echo($row["ItName"].' '.$row["Itdesc"]."<br>");
-        for (){
-            echo ("<img src=uploads\\".$row["imname"].">");
-        }
-    }
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){ 
+    $images=explode(",",$row["imageurls"]); 
+    foreach ($images as $img){ 
+        echo ("<img src=uploads\\".$img." height='100px'>"); 
+    } 
+    echo($row["ItName"].' '.$row["Itdesc"]."<br>"); 
+    echo("<br>"); 
+} 
 
 ?>   
 
