05f94126d451bce4eeca4e6670d2a49128867112
Antuuan <antonycheng2007@gmail.com>
2025-11-10 16:56:22 +0000

    posjdrg

--- index.php
+++ index.php
@@ -21,17 +21,35 @@ include("navbar.php")
         ON tbl_items.item_id = tbl_items_n_pics.item_id
         INNER JOIN tbl_pics
         ON tbl_pics.pic_id = tbl_items_n_pics.pic_id
+        GROUP BY 
+        tbl_items.item_id, tbl_items.item_name, tbl_items.item_description, tbl_items.price;
+
     ");
 	$stmt->execute();	
-    
+?>
+
+<div class="container-fluid">
+
+<?php
 // displays all items
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $count=0;
+        //print_r($row);
         // spliiting the single string of image names into individual names
         $images=explode(",",$row["imageurls"]);
-        echo ("<img src=uploads\\".$images[0]." height='100px'>");
+        // make new row every 5th item
+        if ($count==0){
+            echo("<div class='row'>");
+        }
+        // displays first image and desc in a column
+        echo ("<div class='col-sm3'><img src=uploads\\".$images[0]." height='100px'><br>");
         // display name and description
-        echo($row["ItName"].' '.$row["Itdesc"]."<br>");
+        echo($row["ItName"].' '.$row["Itdesc"]."<br></div>");
         echo("<br>");
+        
+        if ($count==0){
+            echo("</div>");
+        }
     }
 ?>
 
