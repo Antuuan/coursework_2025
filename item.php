c46fd18549a05ff71f63b05ae3ca027be432e1cb
Antuuan <antonycheng2007@gmail.com>
2026-01-14 10:30:47 +0000

    item.php now fetches images

--- item.php
+++ item.php
@@ -9,14 +9,32 @@
     $item_id = (int)$_GET["id"];
     $item_id = $_GET["id"];
 
+// gets item data from DB
     $stmt = $conn->prepare("
-    SELECT tbl_items.*, tbl_users.username
-    FROM tbl_items
-    JOIN tbl_users ON tbl_items.seller_id = tbl_users.user_id
-    WHERE tbl_items.item_id = :item_id
+        SELECT tbl_items.*, tbl_users.username
+        FROM tbl_items
+        JOIN tbl_users ON tbl_items.seller_id = tbl_users.user_id
+        WHERE tbl_items.item_id =:item_id
     ");
     $stmt->bindParam(":item_id", $item_id);
     $stmt->execute();
     $item = $stmt->fetch(PDO::FETCH_ASSOC);
-    print_r($item)
+    print_r($item);
+
+// gets images from DB
+    $stmt = $conn->prepare("
+        SELECT tbl_items_n_pics.item_id,
+        GROUP_CONCAT(tbl_pics.image_name) AS imageurls
+        FROM tbl_items_n_pics
+        INNER JOIN tbl_pics
+        ON tbl_pics.pic_id = tbl_items_n_pics.pic_id
+        WHERE tbl_items_n_pics.item_id=:item_id
+        GROUP BY
+        tbl_items_n_pics.item_id
+    ");
+    $stmt->bindParam(":item_id", $item_id);
+	$stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $images = explode(',', $row['imageurls']);
+    print_r($images)
 ?>
